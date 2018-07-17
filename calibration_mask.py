90a93b3e92861149b51fcf93c83bc6348f7e35ae
f3e7n2g1@gmail.com <f3e7n2g1@gmail.com>
2018-07-16 18:18:46 -0700

    finished all module blocks

--- calibration_mask.py
+++ calibration_mask.py
@@ -15,7 +15,7 @@ def get3DCoor(sizeBoardCorners):
 
 # ------------------------------------
 # find corners of all images from a directory with given corner shape, then caliculate matrix for correcting distortion
-def calDistortMatrix(fileDir, sizeBoardCorners, DEBUG=False):
+def calDistortMatrix(fileDir, sizeBoardCorners, ifPlot=False):
     calFiles = glob.glob("camera_cal/calibration*.jpg")
 
     corners_img = []
@@ -36,14 +36,14 @@ def calDistortMatrix(fileDir, sizeBoardCorners, DEBUG=False):
         corners_img.append(corners)
         corners_tar.append(tarCoord)
         
-        if DEBUG:
+        if ifPlot:
             # draw the board and the found corners to make sure all are found correctly
             cv2.drawChessboardCorners(img, sizeBoardCorners, corners, ret)
             plt.subplot(5, 5, i+1)
             plt.imshow(img)
             i += 1
         
-    if DEBUG:
+    if ifPlot:
         plt.show()
     
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(corners_tar, corners_img, gray.shape, None, None)
@@ -157,10 +157,51 @@ def combineMasks(img):
     # keep strong yellow channel and strong gradient
     combinedMask = (rg_mask > 0) & (gradient_mask > 0)
     
-    # add the s-channel
+    # add the s-channelw
     combinedMask = (combinedMask > 0) | (s_mask > 0)
     
     # cut off outer region and only keep region of interest in the lower middle part of the view
     combinedMask = (combinedMask > 0) & (roi_mask > 0)
     
     return combinedMask
+
+
+# ------------------------------------
+# Perspective transform of a rectangular
+def computePerspective(ifPlot=False):
+    
+    # make a copy of image for drawing
+    testFn = 'test_images/straight_lines1.jpg'
+    img_orig = cv2.imread(testFn)
+    img_orig = cv2.cvtColor(img_orig, cv2.COLOR_BGR2RGB)
+    
+    # Four corner points of the rectangular used to map perspective
+    points_orig = np.array([[312,650], [1007, 650], [720, 470], [568, 470]])
+    points_targ = np.array([[360,700], [850, 700], [850, 100], [360, 100]])
+
+    # Compute the forward and reverse persepective transform matrices
+    M         = cv2.getPerspectiveTransform(np.float32(points_orig), np.float32(points_targ))
+    M_inv = cv2.getPerspectiveTransform(np.float32(points_targ), np.float32(points_orig))
+    
+    # Apply the forward transform to get bird view image
+    img_bird = cv2.warpPerspective(img_orig, M, (img_orig.shape[1], img_orig.shape[0]) , flags=cv2.INTER_LINEAR)
+
+    if ifPlot:
+        # Draw boundaries of the rectangular in both views
+        cv2.polylines(img_orig, np.int32([points_orig]), isClosed=True, color=[255, 0 ,0], thickness=3)
+        cv2.polylines(img_bird, np.int32([points_targ]), isClosed=True, color=[0, 0, 255], thickness=3)
+        
+        # Display
+        f, axhandles = plt.subplots(1, 2, figsize=(20,10))
+
+        axhandles[0].imshow(img_orig)
+        axhandles[0].set_title('Original Image with 4 Source points')
+
+        axhandles[1].imshow(img_bird)
+        axhandles[1].set_title('Bird-view Image with 4 Target points')
+        
+        plt.show()
+        
+    return M, M_inv
+
+# def 
--- laneFinder.py
+++ laneFinder.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 import math
+from calibration_mask import *
 
 class LaneFinder:
     '''
     Initialize the window size and allocate size for window coordinates etc
     '''
-    def __init__(self, num_window=10, width_window = 200):
+    def __init__(self,  M, M_inv, num_window=10, width_window = 200):
         self.isInit = False
         self.threshold_minCountToUpdateCenter = 90
         
@@ -19,6 +20,8 @@ class LaneFinder:
         
         self.y_range = np.zeros((num_window, 2), np.int32)
         
+        self.M = M
+        self.M_inv = M_inv
 
     ''' 
     Run for the first time, check the center of each left and right lane and assign it to the first (lowest) window
@@ -37,13 +40,12 @@ class LaneFinder:
         print('Initial Lane Centers: left = {}, right = {}'.format(self.x_center_llane.item(0),  self.x_center_rlane.item(0)))
 
         self.isInit = True
-
     
     '''
     '''
     def fitWindowCenter(self, img):        
         # new 3-channel mask for drawing
-        self.out_img = np.dstack((img, img, img))*255
+        self.img_laneAndWindow = np.dstack((img, img, img))*255
         
         # current non-zero coordinates
         self.nonzero_x = np.where(img>0)[1]
@@ -86,10 +88,10 @@ class LaneFinder:
                 xwin_r = [center_r-self.WW, center_r+self.WW]
                 
             # Draw the window boundary
-            cv2.rectangle(self.out_img, (xwin_l[0], ywin[0]), (xwin_l[1], ywin[1]),\
-                          color=(255,0,0), thickness=3)
-            cv2.rectangle(self.out_img, (xwin_r[0], ywin[0]), (xwin_r[1], ywin[1]),\
-                          color=(0,0,255), thickness=3)
+            cv2.rectangle(self.img_laneAndWindow, (xwin_l[0], ywin[0]), (xwin_l[1], ywin[1]),\
+                          color=(255,0,0), thickness=5)
+            cv2.rectangle(self.img_laneAndWindow, (xwin_r[0], ywin[0]), (xwin_r[1], ywin[1]),\
+                          color=(0,0,255), thickness=5)
         
         # ------------------------------------------------------------
         # Fit a second order polynomial to each side of the lane, get fitted line center
@@ -112,20 +114,22 @@ class LaneFinder:
         fit_xr = fitcoeff_r[0]*fit_y**2 + fitcoeff_r[1]*fit_y**1 + fitcoeff_r[2]
         points_r = np.stack((fit_xr, fit_y), axis=1)
         
-        cv2.polylines(self.out_img, np.int32([points_l]), isClosed=False, color=(255,0,0), thickness=5)
-        cv2.polylines(self.out_img, np.int32([points_r]), isClosed=False, color=(0,0,255), thickness=5)
+        cv2.polylines(self.img_laneAndWindow, np.int32([points_l]), isClosed=False, color=(255,0,0), thickness=5)
+        cv2.polylines(self.img_laneAndWindow, np.int32([points_r]), isClosed=False, color=(0,0,255), thickness=5)
         
         # ------------------------------------------------------------
         # Compute lane curvature
         self.computeCurvature()
         
         
-    def computeCurvature(self):
-        print(fitcoeff_l, fitcoeff_r)
+    def computeCurvature(self, ifPrintInfo=False):
+        # Compute curvature at the bottom of the view
         curv_l = self.computeSingleCurvature(self.fitcoeff_l, self.sizy)
-        curv_r = self.computeSingleCurvature(self.fitcoeff_r, self.sizy)        
-        print('Left curvature radius {:.1f} meter, Right curvature radius {:.1f} meter'.format(curv_l, curv_r))
-                         
+        curv_r = self.computeSingleCurvature(self.fitcoeff_r, self.sizy)
+        if ifPrintInfo:
+            print('Left curvature radius {:.1f} meter, Right curvature radius {:.1f} meter'.format(curv_l, curv_r))
+
+        self.curv = (curv_l + curv_r)/2
     
     # Input coef is in unit of pixel
     def computeSingleCurvature(self, coef, y_location):
@@ -133,6 +137,7 @@ class LaneFinder:
         x_meter_per_pixel = 3.7/477 # meter/pixel US lane width 3.7 meter, 394~871 in the image
         y_meter_per_pixel = 30/400 # 40 feet, 12.19meters between two dash line, 400 pixel in image
         
+        # Map unit of coefficients and locaiton from pixel to meter
         A = x_meter_per_pixel / (y_meter_per_pixel**2) * coef[0]
         B = x_meter_per_pixel / y_meter_per_pixel * coef[1]
         Y = y_location*y_meter_per_pixel
@@ -144,12 +149,89 @@ class LaneFinder:
     def findPointsIndex(self, xrange, yrange):
         pntIndex = np.where((self.nonzero_x > xrange[0]) & (self.nonzero_x < xrange[1]) & (self.nonzero_y > yrange[0]) & (self.nonzero_y < yrange[1]))
         return np.squeeze(np.array(pntIndex))
+    
+    
+    # 
+    def findCarPosition(self, ifPrintInfo=False):
+        m = np.zeros((self.sizy, self.sizx), np.int32)
+        y = self.sizy - 10
+        x_l = self.fitcoeff_l[0]*y**2 + self.fitcoeff_l[1]*y**1 + self.fitcoeff_l[2]
+        x_l = np.int32(x_l)
+        x_r = self.fitcoeff_r[0]*y**2 + self.fitcoeff_r[1]*y**1 + self.fitcoeff_r[2]
+        x_r =  np.int32(x_r)
         
+        # Set the lane line start points in the bird view
+        m[y, x_l] = 1
+        m[y, x_r] = 1
+        
+        # map this mask to orignal view
+        m_orig = cv2.warpPerspective(np.float64(m), self.M_inv, (self.sizx, self.sizy), flags=cv2.INTER_LINEAR)
+
+        # find line start points in original view
+        nonzero_x = np.where(m_orig>0)[1]
+        center_l = np.median(nonzero_x[nonzero_x < self.sizx/2])
+        center_r = np.median(nonzero_x[nonzero_x > self.sizx/2])
+        
+        # calculate car location
+        ratio = 3.7/(center_r-center_l) # meter/pixel (3.7 meter lane width)
+        c_shift_pixel = (center_l+center_r)/2 - self.sizx/2
+        self.c_shift_meter = c_shift_pixel*ratio
+        
+        if ifPrintInfo:
+            print('line start pixel index : l {}, r {}'.format(center_l, center_r))
+            print('lane center shifts:  {:.1f} pixels, or {:.3f} meters'.format(c_shift_pixel, self.c_shift_meter))
+        
+    # origImg is RGB orignal image
+    def drawLaneBoundaryInOrigView(self, origImg):
+        m = np.zeros((self.sizy, self.sizx, 3), np.float64)
+        
+        # Draw the fitted lines and regions in between
+        fit_y = np.array(range(self.sizy), np.int32)        
+        fit_xl = self.fitcoeff_l[0]*fit_y**2 + self.fitcoeff_l[1]*fit_y**1 + self.fitcoeff_l[2]
+        points_l = np.stack((fit_xl, fit_y), axis=1)        
+        fit_xr = self.fitcoeff_r[0]*fit_y**2 + self.fitcoeff_r[1]*fit_y**1 + self.fitcoeff_r[2]
+        points_r = np.stack((fit_xr, fit_y), axis=1)
+        
+        cv2.polylines(m, np.int32([points_l]), isClosed=False, color=(255,0,0), thickness= 25)
+        cv2.polylines(m, np.int32([points_r]), isClosed=False, color=(0,0,255), thickness= 25)
+        
+        corners = np.zeros((4,2))
+        corners[0:2, :] = points_l[[1, -1], :]
+        corners[0:2, 0] += 5
+        corners[2:4, :] = points_r[[-1, 1], :]
+        corners[2:4, 0] -= 5
+        cv2.fillPoly(m, np.int32([corners]), color=(0, 230, 0))
+        
+        # Wrap it into orignal view
+        fills_origView = cv2.warpPerspective(m, self.M_inv, (self.sizx, self.sizy), flags=cv2.INTER_LINEAR)
+    
+        # Add two images together
+        raw = np.float64(cv2.cvtColor(origImg, cv2.COLOR_BGR2RGB)/255)
+        outweighted = cv2.addWeighted(raw, 1,  fills_origView, 0.5, 0)
+        
+        return outweighted
+        
+    # Display info in output image
+    def write_Info(self):
+        font = cv2.FONT_HERSHEY_DUPLEX
+        
+        textCurv = 'Radius (curvature): {:.2f} m'.format(self.curv)
+        
+        if self.c_shift_meter > 0:
+            side = 'right'
+        else:
+            side = 'left'
+        textLoca = 'Location: {:.3} m {} of center'.format(abs(self.c_shift_meter), side)
+
+        # cv2.putText(img, text, org, fontFace, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]]) → None¶
+
+        cv2.putText(self.final, text=textCurv, org=(40,70), fontFace=font, fontScale=1.5, color=(255,255,0), thickness=2)
+        cv2.putText(self.final, text=textLoca, org=(40,120), fontFace=font, fontScale=1.5, color=(255,255,0), thickness=2)
         
     # INPUT: 
     # img should be 2D grayscale, bird-view image of lanes from combined mask
     # num_window and width_window define the sliding window
-    def run(self, mask):
+    def run(self, origImg, mask):
         # Initialize 
         if not self.isInit:
             self.init(mask)
@@ -157,3 +239,15 @@ class LaneFinder:
         # Find window centers
         self.fitWindowCenter(mask)
         
+        # Compute lane curvature
+        self.computeCurvature()
+        
+        # Find the location of  the car within lane
+        self.findCarPosition()
+        
+        # Draw lane boundary and regions in original view
+        self.final = self.drawLaneBoundaryInOrigView(origImg)
+        
+        # Print information on final image
+        self.write_Info()
+        return self.final
